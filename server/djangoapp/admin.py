ecdd8621d8104a1d766fd45c6562a2559e758f36
ezgilenas <ezgilena00@gmail.com>
2024-11-08 22:47:27 +0300

    I built CarMake and CarModel models in Django model and did migrations and wrote the ...migrate --run-syncdb. Aslinda view.pya yazdigim get_cars(request) fonksiyonunda arabalari listelemek adina yazdigim kod, pathteki 127.0.0.1:8000/djangoapp/get_cars ile acilabiliyor ve acildiginda json dosyasi gibi gorunuyor, onu anlamadim neden oyle oldu.

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,12 @@
-# from django.contrib import admin
-# from .models import related models
-
+from django.contrib import admin
+#####from .models import related models
+from .models import CarMake, CarModel
+#I uncommented the imports above
 
 # Register your models here.
+#Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,9 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+#I uncommented the all imports above
 
 # Create your models here.
 
@@ -23,3 +23,31 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    #Other fields as needed
+
+    def __str__(self):
+        return self.name 
+        #Return the name as the string representation
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    #Many-to-One relationship is above
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV','Suv'),
+        ('WAGON','Wagon'),
+        #Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[MaxValueValidator(2023),
+        MinValueValidator(2015)])
+    #Other fields as needed
+
+    def __str__(self):
+        return self.name
+        #Return the name as the string representation
